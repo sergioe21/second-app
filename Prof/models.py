5706019a877b531ccb68b7098087f04c584fc617
sergioe21 <sergioesteban.21@gmail.com>
2016-01-22 18:19:19 -0500

    email

--- Prof/models.py
+++ Prof/models.py
@@ -9,6 +9,7 @@ class worker(models.Model):
 	last_name = models.CharField(max_length=200)
 	first_name = models.CharField(max_length=200)
 	age = models.IntegerField(default = 0)
+	email = models.EmailField(max_length=100)
 
 	def __str__(self):
 		comp = self.first_name + ' ' +self.last_name
